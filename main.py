18f55c3bb5209976a7cfc532fe47ddcf52cd363d
Isabelly Nunes <isanunes259@gmail.com>
2024-11-24 20:55:43 -0300

    salvando o meu arquivo main

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Olá mundo")
+
+# Variáveis
+nome = "Isabelly"
+idade = 28
+print(nome,idade)
+
+# Soma
+
+idade_nova = idade + 1
+print(idade_nova)
+
+# Lista
+
+lista = [1,2,3,4,5]
+
+# Métodos
+
+lista_inversa = lista.reverse
+
+
